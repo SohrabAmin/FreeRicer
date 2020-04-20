abaede6f9cdee1c599cd77d0c20b90ef25a18588
aishahkabir <aishahkab@gmail.com>
2020-04-20 18:41:30 -0400

    Add Class Question to main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import selenium
 
 
+class Question:
+    """
+    A Question Object
+
+    === Attributes ===
+    answers: list
+    """
+
+    def __init__(self, answers):
+        self.answers = answers
+
+
 if __name__ == '__main__':
     pass
