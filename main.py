fdf94ff3d75f923be672baa30774cf81148193aa
SohrabAmin <sohrobo@hotmail.com>
2020-04-20 20:02:49 -0400

    Creates question object from freerice.com

--- main.py
+++ main.py
@@ -18,6 +18,17 @@ def click_element(driver, xpath: str) -> None:
         print('Can Not Find element with XPATH: ' + xpath)
 
 
+def get_text(driver, xpath:str) -> str:
+    """
+    Find and Return the value of the HTML element with <xpath>
+    """
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, xpath)))
+        return element.get_attribute('innerHTML')
+    except NoSuchElementException:
+        print('Can Not Find element with XPATH: ' + xpath)
+
 class Question:
     """
     A Question Object
@@ -45,7 +56,6 @@ class Question:
 
 
 if __name__ == '__main__':
-
     # Create a driver to access the web
     driver_path = os.getcwd() + '/geckodriver'
 
@@ -58,5 +68,18 @@ if __name__ == '__main__':
     # Click 'OK' on cookie notification
     click_element(driver, '/html/body/div[1]/div/div/div[2]/div[1]/button')
 
-    #Click on 'Basic Math (Pre-Algebra)' icon
+    # Click on 'Basic Math (Pre-Algebra)' icon
     click_element(driver, '/html/body/div[2]/section/div/div[1]/div/div[2]/div/div/div[7]/div[2]/div[1]')
+
+    # Find the question
+    question = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[1]')
+
+    # Find the answers
+    ans1 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[2]')
+    ans2 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[3]')
+    ans3 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[4]')
+    ans4 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[5]')
+
+    q = Question(question, [ans1, ans2, ans3, ans4])
+
+    print(q.solver in q.answers)
