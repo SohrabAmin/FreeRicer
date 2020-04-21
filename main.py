46bd89ef449eae12bbe0417c399d448d07c5da9b
SohrabAmin <sohrobo@hotmail.com>
2020-04-21 18:41:07 -0400

    Continuously loop to new questions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+import random
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -10,12 +12,8 @@ def click_element(driver, xpath: str) -> None:
     """
     Find an HTML element with <xpath> and clicks it.
     """
-    try:
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, xpath)))
-        element.click()
-    except NoSuchElementException:
-        print('Can Not Find element with XPATH: ' + xpath)
+    element = driver.find_element_by_xpath(xpath)
+    driver.execute_script("arguments[0].click();", element)
 
 
 def get_text(driver, xpath: str) -> str:
@@ -42,7 +40,7 @@ class Question:
         self.question = question
         self.answers = answers
 
-    def solver(self) -> int:
+    def solver(self) -> str:
         """
         Looks at question attribute in Question object and
         provides solution from answers attribute
@@ -70,18 +68,31 @@ if __name__ == '__main__':
     # Click on 'Basic Math (Pre-Algebra)' icon
     click_element(driver, '/html/body/div[2]/section/div/div[1]/div/div[2]/div/div/div[7]/div[2]/div[1]')
 
-    # Find the question
-    question = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[1]')
+    # Loop Forever
+    while True:
+        # Find the question
+        question = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div['
+                                    '1]/div/div/div/div/div/div[1]')
 
-    # Find the answers
-    ans1 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[2]')
-    ans2 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[3]')
-    ans3 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[4]')
-    ans4 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[5]')
+        # Find the answers
+        ans1 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[2]')
+        ans2 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[3]')
+        ans3 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[4]')
+        ans4 = get_text(driver, '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div[5]')
 
-    # Create question object
-    q = Question(question, [ans1, ans2, ans3, ans4])
+        # Create question object
+        q = Question(question, [ans1, ans2, ans3, ans4])
 
-    # Select option that is correct
-    ans_location = '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div'
-    click_element(driver, ans_location + "[%d]" % (q.answers.index(q.solver())+2))
+        # Select option that is correct, otherwise a random one if answer unknown
+        ans_location = '/html/body/div[2]/section/div/div[1]/div/div/div[4]/div[1]/div/div/div/div/div/div'
+        if q.solver() in q.answers:
+            click_element(driver, ans_location + "[%d]" % (q.answers.index(q.solver())+2))
+
+        else:
+            click_element(driver, ans_location + "[%d]" % random.randint(2, 5))
+
+        # Avoid too may request error
+        time.sleep(1)
+
+
+
